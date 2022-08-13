edfc9752f49fc8e9bbb626acf46faa00a4891e44
Travis Kaufman <travis.kaufman@gmail.com>
2022-08-13 17:48:49 -0400

    alignItems center

--- src/App.tsx
+++ src/App.tsx
@@ -25,11 +25,11 @@ function App() {
             value={seq().toString()}
             onChange={(ev) => setRawSeq(ev.target.value)}
           />
-          <Stack direction="row" spacing={4}>
+          <Stack direction="row" spacing={4} alignItems="center">
             <span>Complement:</span>
             <Seq seq={complement()} />
           </Stack>
-          <Stack direction="row" spacing={4}>
+          <Stack direction="row" alignItems="center" spacing={4}>
             <span>Reverse Complement:</span>
             <Seq seq={reverseComplement()} />
           </Stack>
